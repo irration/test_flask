962343397cd75b2574e7e86082dbc71e87ab6620
irration <nisei.kimura@gmail.com>
2018-03-11 22:46:47 +0900

    routing test

--- flask_apps/hello-app/hello.py
+++ flask_apps/hello-app/hello.py
@@ -4,10 +4,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return "Index Page"
+
+@app.route('/hello')
 def hello_world():
-	return 'Hello World!'
+	i = 3
+	return "You've visited " + str(i) + " times"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+	# show the user profile for that user
+	return "User %s" % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	return "Post %d" % post_id
 
 if __name__ == '__main__':
 	host = os.getenv('IP', '0.0.0.0')
 	port = int(os.getenv('PORT', 5000))
+	app.debug = True
 	app.run(host=host, port=port)
