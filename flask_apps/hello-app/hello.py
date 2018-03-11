15b7711bf8ff66ab53dba044bbe07f4f58a4d160
irration <nisei.kimura@gmail.com>
2018-03-12 01:42:29 +0900

    Implemented Login / Logout

--- flask_apps/hello-app/hello.py
+++ flask_apps/hello-app/hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
 
 app = Flask(__name__)
 
@@ -9,17 +9,29 @@ def login(post=None):
 	if request.method == 'POST':
 		if valid_login(request.form['username'], request.form['password']):
 			flash('Successfully logged in')
-			return redirect(url_for('welcome', username=request.form.get('username')))
+			response = make_response(redirect(url_for('welcome', username=request.form.get('username'))))
+			response.set_cookie('username', request.form.get('username'))
+			return response
 		else:
 			error = "Username or password incollect"
 	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	flash('Successfully Logged out.')
+	response = make_response(redirect(url_for('login')))
+	response.set_cookie('username', '', expires=0)
+	return response
 
 def valid_login(username, password):
 	return username == password
 
 @app.route('/welcome/<username>')
 def welcome(username):
-	return render_template('welcome.html', username=username)
+	username = request.cookies.get('username')
+	if username:
+		return render_template('welcome.html', username=username)
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	host = os.getenv('IP', '0.0.0.0')
