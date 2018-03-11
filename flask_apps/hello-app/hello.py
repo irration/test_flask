c34a09aa0a56f5e1d7fac8ea8f952dd62e85c79b
irration <nisei.kimura@gmail.com>
2018-03-12 00:50:39 +0900

    template inheritance

--- flask_apps/hello-app/hello.py
+++ flask_apps/hello-app/hello.py
@@ -1,15 +1,29 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html', name=name)
+@app.route('/login', methods=['GET', 'POST'])
+def login(post=None):
+	error = None
+	if request.method == 'POST':
+		if valid_login(request.form['username'], request.form['password']):
+			flash('Successfully logged in')
+			return redirect(url_for('welcome', username=request.form.get('username')))
+		else:
+			error = "Username or password incollect"
+	return render_template('login.html', error=error)
+
+def valid_login(username, password):
+	return username == password
+
+@app.route('/welcome/<username>')
+def welcome(username):
+	return render_template('welcome.html', username=username)
 
 if __name__ == '__main__':
 	host = os.getenv('IP', '0.0.0.0')
 	port = int(os.getenv('PORT', 5000))
 	app.debug = True
+	app.secret_key = 'SuperSecretKey'
 	app.run(host=host, port=port)
